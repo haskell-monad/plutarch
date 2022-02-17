c75385bcb7c8e647cc3c4f6d2056e801f1e283dd
Geometer1729 <16kuhnb@gmail.com>
2022-02-17 09:49:56 -0500

    update flake nameing consistancy

--- Plutarch/List.hs
+++ Plutarch/List.hs
@@ -9,7 +9,7 @@ module Plutarch.List (
   -- * Comparison
   plistEquals,
   psort,
-  pmergesort,
+  pmergeSort,
   ptimSort,
 
   -- * Query
@@ -456,8 +456,8 @@ psort = ptimSort
 -- TODO: decide on default sort
 -- probably by benchmarking them
 
-pmergesort :: POrd a => Term s (PList a :--> PList a)
-pmergesort = phoistAcyclic $
+pmergeSort :: POrd a => Term s (PList a :--> PList a)
+pmergeSort = phoistAcyclic $
   pfix #$ plam $ \self xs ->
     pmatch xs $ \case
       PSNil -> pcon PSNil
